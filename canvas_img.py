74f1d37dde44babfb787ef16b0f20677b01da522
mustafabiyik <69960156+mustafabiyik@users.noreply.github.com>
2020-12-27 01:13:46 +0300

    Update canvas_img.py

--- canvas_img.py
+++ canvas_img.py
@@ -1,14 +1,14 @@
 from tkinter import *
 
 root = Tk()
-root.title('Codemy.com')
-root.iconbitmap('c:/gui/codemy.ico')
-root.geometry("800x600")
+root.title('museum map') 
+root.iconbitmap('c:/gui/codemy.ico')#put here the map which is drawn by hand
+root.geometry("800x600")//?
 
-w = 600
-h = 400
-x = w/2
-y = h/2
+w = 5.4#x coordibnates we can multiply with suitable values with the same ratio
+h = 3#y coordinates
+x = 0#x points the origin
+y = 0#y points the origin
 
 my_canvas = Canvas(root, width=w, heigh=h, bg="white")
 my_canvas.pack(pady=20)
@@ -16,12 +16,13 @@ my_canvas.pack(pady=20)
 
 
 # Add Image To Canvas
-img = PhotoImage(file="c:/gui/images/me.png")
-my_image = my_canvas.create_image(260,125, anchor=NW, image=img)
+img = PhotoImage(file="c:/gui/images/me.png")#upload moving photo here 
+my_image = my_canvas.create_image(0,0, anchor=NW, image=img)#image which is moving is initially at the origin
 
-
+#create two variable then compare them with the previous position update the cuurent location
+#
 def left(event):
-	x = -10
+	x = -10 #
 	y = 0
 	my_canvas.move(my_image, x, y)
 
